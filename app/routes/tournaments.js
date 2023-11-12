17265d51dcbf8a4df6c4bb14c285ab786f584a66
TheRazzBerry <razeen.hrahman@gmail.com>
2023-11-11 23:50:00 -0500

    Backend update

--- app/routes/tournaments.js
+++ app/routes/tournaments.js
@@ -15,13 +15,18 @@ let authController = require('../controllers/authController');
 router.get('/', (req, res, next) => { res.json({ "message" : "tournaments.js root directory" }); });
 router.get('/list', tournamentController.list);
 router.get('/:id', tournamentController.find, tournamentController.read);
-router.post('/', tournamentController.add);
 
+router.post('/create', 
+    authController.requireSignin,
+    tournamentController.add);
 router.put('/edit/:id', 
     authController.requireSignin,
     authController.isAllowed,
     tournamentController.update);
-router.delete('/:id', tournamentController.delete);
+router.delete('/:id', 
+    authController.requireSignin,
+    authController.isAllowed,
+    tournamentController.delete);
 
 // Export Module
 module.exports = router;
