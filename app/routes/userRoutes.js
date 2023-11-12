6f725291d1a471dcb7170bb820370d96fa8cf562
pateldev11 <dev95277@gmail.com>
2023-11-11 22:54:07 -0500

    update UserRoutes

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -31,4 +31,70 @@ router.patch('/profile', authenticate, async (req, res) => {
   }
 });
 
+// Get all users
+router.get('/users', authenticate, async (req, res) => {
+  try {
+    const users = await userModel.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Get user by ID
+router.get('/users/:id', authenticate, async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await userModel.findById(id);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Update user by ID
+router.patch('/users/:id', authenticate, async (req, res) => {
+  const { id } = req.params;
+  const allowedUpdates = ['userName', 'email', 'password'];
+  const updates = Object.keys(req.body);
+
+  const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+  if (!isValidOperation) {
+    return res.status(400).json({ error: 'Invalid updates!' });
+  }
+
+  try {
+    const user = await userModel.findById(id);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    updates.forEach((update) => (user[update] = req.body[update]));
+    await user.save();
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Delete user by ID
+router.delete('/users/:id', authenticate, async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await userModel.findByIdAndDelete(id);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.status(200).json({ message: `User with id ${id} deleted` });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// ... (more routes if needed)
+
 module.exports = router;
