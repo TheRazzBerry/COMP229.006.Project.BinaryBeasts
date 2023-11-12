10ec11ffa35195a1e09b62c01acf98f4dd22df25
pateldev11 <dev95277@gmail.com>
2023-11-11 21:51:15 -0500

    CREATED AUTHCONTROLLER AND AUTHORIZATIONAN

--- app/models/userModel.js
+++ app/models/userModel.js
@@ -1,91 +1,94 @@
-// Define Module Dependencies
+// models/userModel.js
+
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-// Create Schema
 const userSchema = mongoose.Schema({
-    userName: {
-        type: String,
-        unique: true,
-        required: 'Username is required',
-        trim: true
-      },
-
-    email: {
+  userName: {
     type: String,
     unique: true,
-    match: [/.+\@.+\..+/, "Please fill a valid e-mail address"]
+    required: 'Username is required',
+    trim: true,
   },
-
-    hashedPass: {
+  email: {
     type: String,
-    required: 'Passowrd is required',
+    unique: true,
+    match: [/.+\@.+\..+/, 'Please fill a valid e-mail address'],
   },
-
+  hashedPass: {
+    type: String,
+    required: 'Password is required',
+  },
   salt: {
-    type: String
+    type: String,
   },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
   created: {
     type: Date,
     default: Date.now,
-    immutable: true
+    immutable: true,
   },
   updated: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
   admin: {
     type: Boolean,
-    default: false
-  }
-},
-  {
-    collection: "user"
-  }
-);
+    default: false,
+  },
+});
 
-userSchema.virtual('fullName')
-  .get(function () {
-    return this.firstName + ' ' + this.lastName;
-  })
-  .set(function (fullName) {
-    let splitName = fullName.split(' ');
-    this.firstName = splitName[0] || '';
-    this.lastName = splitName[1] || '';
-  });
-
-
-  userSchema.virtual('password')
-  .set(function (password) {
-    if (password.length < 8) {
-      throw new Error('Password must be at least 8 characters.')
-    }
-    else {
-      this.salt = Buffer.from(crypto.randomBytes(16).toString('base64'), 'base64');
-      this.hashedPass = this.hashPassword(password);
-    }
-  });
-
-  userSchema.methods.hashPassword = function (password) {
-  return crypto.pbkdf2Sync(password, this.salt, 10000, 64, 'sha512').toString('base64');
-}
-
-userSchema.methods.authenticate = function (password) {
-  return this.hashedPass === this.hashPassword(password);
-}
-
-
-// Ensure virtual fields are serialised.
-userSchema.set('toJSON', {
-  virtuals: true,
-  versionKey:false,
-  transform: function (doc, ret) { 
-    delete ret.hashedPass;
-    delete ret.salt;
+userSchema.virtual('password').set(function (password) {
+  if (password.length < 8) {
+    throw new Error('Password must be at least 8 characters.');
+  } else {
+    this.salt = bcrypt.genSaltSync(10);
+    this.hashedPass = bcrypt.hashSync(password, this.salt);
   }
 });
 
-// Define Model
-const user = mongoose.model('User', userSchema);
+userSchema.methods.generateAuthToken = async function () {
+  const token = jwt.sign({ _id: this._id.toString() }, 'your-secret-key'); // Replace 'your-secret-key' with your actual secret key
+  this.tokens = this.tokens.concat({ token });
+  await this.save();
+  return token;
+};
 
-// Export Model
-module.exports = user;
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error('Invalid login credentials');
+  }
+
+  const isPasswordMatch = await bcrypt.compare(password, user.hashedPass);
+
+  if (!isPasswordMatch) {
+    throw new Error('Invalid login credentials');
+  }
+
+  return user;
+};
+
+userSchema.methods.toJSON = function () {
+  const user = this;
+  const userObject = user.toObject();
+
+  delete userObject.hashedPass;
+  delete userObject.salt;
+  delete userObject.tokens;
+
+  return userObject;
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- config/app.js
+++ config/app.js
@@ -2,35 +2,47 @@
 const createError = require('http-errors');
 const logger = require('morgan');
 const express = require('express');
+const bodyParser = require('body-parser'); // Add body-parser
 
 // Define Express Application
-var app = express();
+const app = express();
 
 // Define Routers
-const indexRouter = require('../app/routes/index.js');
-const usersRouter = require('../app/routes/users.js');
-const tournamentRouter = require('../app/routes/tournaments.js');
+const indexRouter = require('../app/routes/index.js'); // Adjust the path accordingly
+const authRoutes = require('../app/routes/authRoutes.js');
+const userRoutes = require('../app/routes/userRoutes.js');
+const tournamentRouter = require('../app/routes/tournaments.js'); // Assuming you have a tournament router
 
 // Define Middleware Routes
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json()); // Use body-parser middleware for JSON parsing
 
 // Define Page Routes
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+// Use authentication routes
+app.use('/auth', authRoutes);
+
+// Use user routes
+app.use('/user', userRoutes);
+
+// Use tournament routes
 app.use('/tournaments', tournamentRouter);
 
 // Catch Error 404
-app.use(function (req, res, next) { next(createError(404)); });
-
-// Error Handler
-app.use(function (error, req, res, next) {
-    console.log(error);
-    // Send Error Response
-    res.status(error.status || 500);
-    res.json({ success: false, message: error.message });
+app.use((req, res, next) => {
+  next(createError(404));
 });
 
-//Export Module
+// Error Handler
+app.use((error, req, res, next) => {
+  console.error(error);
+  // Send Error Response
+  res.status(error.status || 500);
+  res.json({ success: false, message: error.message });
+});
+
+// Export Module
 module.exports = app;
